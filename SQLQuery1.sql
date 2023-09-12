0f0bb96519cf67f8f88ed32a8a955b087cde83fe
hieu3557 <124872187+hieu3557@users.noreply.github.com>
2023-09-12 11:19:25 +0700

    Add files via upload
    Query 2 la vi du
    Query 1 la btap

---
+++ SQLQuery1.sql
@@ -0,0 +1,191 @@
+﻿-- Tạo bảng Toys
+CREATE TABLE Toys (
+    ProductCode varchar(5) PRIMARY KEY,
+    Name varchar(30),
+    Category varchar(30),
+    QtyOnHand int
+);
+
+-- Thêm dữ liệu vào bảng Toys
+INSERT INTO Toys (ProductCode, Name, Category, QtyOnHand)
+VALUES
+    ('T001', 'Lego Set 1', 'Lego', 25),
+    ('T002', 'Board Game 1', 'Board Game', 30),
+    ('T003', 'Puzzle 1', 'Puzzle', 22),
+    ('T004', 'Action Figure 1', 'Action Figure', 28),
+    ('T005', 'Building Blocks 1', 'Building Blocks', 26),
+    -- Thêm dữ liệu cho 10 sản phẩm khác ở đây
+    ('T015', 'Remote Control Car 1', 'RC Car', 21);
+-- Tạo thủ tục lưu trữ HeavyToys
+CREATE PROCEDURE HeavyToys AS
+BEGIN
+    SELECT * FROM Toys
+    WHERE Category = 'Action Figure' AND QtyOnHand > 500;
+END;
+-- Tạo thủ tục lưu trữ PriceIncrease
+CREATE PROCEDURE PriceIncrease AS
+BEGIN
+    UPDATE Toys
+    SET QtyOnHand = QtyOnHand + 10;
+END;
+-- Tạo thủ tục lưu trữ QtyOnHand
+CREATE PROCEDURE QtyOnHand AS
+BEGIN
+    UPDATE Toys
+    SET QtyOnHand = QtyOnHand - 5;
+END;
+-- Thực thi thủ tục lưu trữ HeavyToys
+EXEC HeavyToys;
+
+-- Thực thi thủ tục lưu trữ PriceIncrease
+EXEC PriceIncrease;
+
+-- Thực thi thủ tục lưu trữ QtyOnHand
+EXEC QtyOnHand;
+-- Xem định nghĩa của thủ tục lưu trữ HeavyToys
+EXEC sp_helptext 'HeavyToys';
+
+-- Xem định nghĩa của thủ tục lưu trữ PriceIncrease
+EXEC sp_helptext 'PriceIncrease';
+
+-- Xem định nghĩa của thủ tục lưu trữ QtyOnHand
+EXEC sp_helptext 'QtyOnHand';
+-- Xem định nghĩa của thủ tục lưu trữ HeavyToys
+SELECT OBJECT_DEFINITION(OBJECT_ID('HeavyToys'));
+
+-- Xem định nghĩa của thủ tục lưu trữ PriceIncrease
+SELECT OBJECT_DEFINITION(OBJECT_ID('PriceIncrease'));
+
+-- Xem định nghĩa của thủ tục lưu trữ QtyOnHand
+SELECT OBJECT_DEFINITION(OBJECT_ID('QtyOnHand'));
+
+EXEC sp_depends 'HeavyToys';
+EXEC sp_depends 'PriceIncrease';
+EXEC sp_depends 'QtyOnHand';
+
+-- Chỉnh sửa thủ tục PriceIncrease
+-- Chỉnh sửa thủ tục PriceIncrease
+-- Chỉnh sửa thủ tục PriceIncrease
+ALTER TABLE Toys
+ADD UnitPrice DECIMAL(10, 2); -- Thay đổi kiểu dữ liệu và độ chính xác theo yêu cầu của bạn
+
+
+ALTER PROCEDURE PriceIncrease AS
+BEGIN
+    UPDATE Toys
+    SET QtyOnHand = QtyOnHand + 10, UnitPrice = UnitPrice + 10; -- Thay đổi UnitPrice theo yêu cầu
+END;
+
+-- Chỉnh sửa thủ tục QtyOnHand
+ALTER PROCEDURE QtyOnHand AS
+BEGIN
+    UPDATE Toys
+    SET QtyOnHand = QtyOnHand - 5, UnitPrice = UnitPrice + 10; -- Thay đổi UnitPrice theo yêu cầu
+END;
+
+-- Tạo thủ tục lưu trữ SpecificPriceIncrease
+
+CREATE PROCEDURE SpecificPriceIncrease AS
+BEGIN
+    DECLARE @ProductCode varchar(5);
+    DECLARE @QtyOnHand int;
+    
+    -- Lấy thông tin sản phẩm cần tăng giá
+    SELECT TOP 1 @ProductCode = ProductCode, @QtyOnHand = QtyOnHand
+    FROM Toys
+    WHERE QtyOnHand > 0; -- Chỉ chọn sản phẩm có số lượng còn lại
+    
+    -- Tăng giá của sản phẩm dựa trên số lượng còn lại
+    UPDATE Toys
+    SET UnitPrice = UnitPrice + @QtyOnHand
+    WHERE ProductCode = @ProductCode;
+END;
+-- Chỉnh sửa thủ tục lưu trữ SpecificPriceIncrease
+
+ALTER PROCEDURE SpecificPriceIncrease
+AS
+BEGIN
+    DECLARE @TotalUpdated INT;
+
+    -- Lấy tổng số sản phẩm đã được cập nhật
+    SELECT @TotalUpdated = COUNT(*) 
+    FROM Toys
+    WHERE QtyOnHand > 0; -- Chỉ cập nhật sản phẩm có số lượng còn lại
+
+    -- Cập nhật giá của sản phẩm dựa trên số lượng còn lại
+    UPDATE Toys
+    SET UnitPrice = UnitPrice + QtyOnHand
+    WHERE QtyOnHand > 0;
+
+    -- Trả về tổng số bản ghi đã được cập nhật
+    SELECT @TotalUpdated AS 'TotalUpdatedRecords';
+END;
+
+-- Chỉnh sửa thủ tục lưu trữ SpecificPriceIncrease
+ALTER PROCEDURE SpecificPriceIncrease
+AS
+BEGIN
+    DECLARE @TotalUpdated INT;
+
+    -- Lấy tổng số sản phẩm đã được cập nhật
+    SELECT @TotalUpdated = COUNT(*) 
+    FROM Toys
+    WHERE QtyOnHand > 0; -- Chỉ cập nhật sản phẩm có số lượng còn lại
+
+    -- Gọi thủ tục lưu trữ HeavyToys để liệt kê sản phẩm nặng
+    EXEC HeavyToys;
+
+    -- Cập nhật giá của sản phẩm dựa trên số lượng còn lại
+    UPDATE Toys
+    SET UnitPrice = UnitPrice + QtyOnHand
+    WHERE QtyOnHand > 0;
+
+    -- Trả về tổng số bản ghi đã được cập nhật
+    SELECT @TotalUpdated AS 'TotalUpdatedRecords';
+END;
+
+-- Chỉnh sửa thủ tục lưu trữ SpecificPriceIncrease với xử lý lỗi
+ALTER PROCEDURE SpecificPriceIncrease
+AS
+BEGIN
+    BEGIN TRY
+        DECLARE @TotalUpdated INT;
+
+        -- Lấy tổng số sản phẩm đã được cập nhật
+        SELECT @TotalUpdated = COUNT(*) 
+        FROM Toys
+        WHERE QtyOnHand > 0; -- Chỉ cập nhật sản phẩm có số lượng còn lại
+
+        -- Gọi thủ tục lưu trữ HeavyToys để liệt kê sản phẩm nặng
+        EXEC HeavyToys;
+
+        -- Cập nhật giá của sản phẩm dựa trên số lượng còn lại
+        UPDATE Toys
+        SET UnitPrice = UnitPrice + QtyOnHand
+        WHERE QtyOnHand > 0;
+
+        -- Trả về tổng số bản ghi đã được cập nhật
+        SELECT @TotalUpdated AS 'TotalUpdatedRecords';
+    END TRY
+    BEGIN CATCH
+        -- Xử lý lỗi ở đây
+        SELECT ERROR_MESSAGE() AS 'ErrorMessage';
+    END CATCH;
+END;
+
+-- Xóa bỏ thủ tục lưu trữ HeavyToys
+DROP PROCEDURE IF EXISTS HeavyToys;
+
+-- Xóa bỏ thủ tục lưu trữ PriceIncrease
+DROP PROCEDURE IF EXISTS PriceIncrease;
+
+-- Xóa bỏ thủ tục lưu trữ QtyOnHand
+DROP PROCEDURE IF EXISTS QtyOnHand;
+
+-- Xóa bỏ thủ tục lưu trữ SpecificPriceIncrease
+DROP PROCEDURE IF EXISTS SpecificPriceIncrease;
+
+
+
+
+
---
+++ SQLQuery2.sql
@@ -0,0 +1,170 @@
+﻿USE AdventureWorks2022
+GO
+CREATE PROCEDURE sp_DisplayEmployeesHireYear
+@HireYear int
+AS
+SELECT * FROM HumanResources.Employee
+WHERE DATEPART(YY, HireDate)=@HireYear
+GO
+EXECUTE sp_DisplayEmployeesHireYear 2008
+GO
+
+
+CREATE PROCEDURE sp_EmployeesHireYearCount
+@HireYear int,
+@Count int OUTPUT
+AS
+SELECT @Count=COUNT(*) FROM HumanResources.Employee
+WHERE DATEPART(YY, HireDate)=@HireYear
+GO
+DECLARE @Number int
+EXECUTE sp_EmployeesHireYearCount 2008, @Number OUTPUT
+PRINT @Number
+GO
+
+CREATE PROCEDURE sp_EmployeesHireYearCount2
+@HireYear int
+AS
+DECLARE @Count int
+SELECT @Count=COUNT(*) FROM HumanResources.Employee
+WHERE DATEPART(YY, HireDate)=@HireYear
+RETURN @Count
+GO
+
+DECLARE @Number int
+EXECUTE @Number = sp_EmployeesHireYearCount2 2008
+PRINT @Number
+GO
+
+CREATE TABLE #Students
+(
+RollNo varchar(6) CONSTRAINT PK_Students PRIMARY KEY,
+FullName nvarchar(100),
+Birthday datetime constraint DF_StudentsBirthday DEFAULT 
+DATEADD(yy, -18, GETDATE())
+)
+GO
+--Tạo thủ tục lưu trữ tạm để chèn dữ liệu vào bảng tạm
+CREATE PROCEDURE #spInsertStudents
+@rollNo varchar(6),
+@fullName nvarchar(100),
+@birthday datetime
+AS BEGIN
+IF(@birthday IS NULL)
+SET @birthday=DATEADD(YY, -18, GETDATE())
+INSERT INTO #Students(RollNo, FullName, Birthday)
+VALUES(@rollNo, @fullName, @birthday)
+END
+go
+
+EXEC #spInsertStudents 'A12345', 'abc', NULL
+EXEC #spInsertStudents 'A54321', 'abc', '12/24/2011'
+SELECT * FROM #Students
+--Tạo thủ tục lưu trữ tạm để xóa dữ liệu từ bảng tạm theo RollNo
+CREATE PROCEDURE #spDeleteStudents
+@rollNo varchar(6)
+AS BEGIN
+DELETE FROM #Students WHERE RollNo=@rollNo
+END
+--Xóa dữ liệu sử dụng thủ tục lưu trữ
+EXECUTE #spDeleteStudents 'A12345'
+GO
+
+CREATE PROCEDURE Cal_Square @num int=0 AS
+BEGIN
+RETURN (@num * @num);
+END
+GO
+--Chạy thủ tục lưu trữ
+DECLARE @square int;
+EXEC @square = Cal_Square 10;
+PRINT @square;
+GO
+
+SELECT 
+OBJECT_DEFINITION(OBJECT_ID('HumanResources.uspUpdateEmployeePersonalInfo')) AS DEFINITION
+--Xem định nghĩa thủ tục lưu trữ bằng
+SELECT definition FROM sys.sql_modules
+WHERE 
+object_id=OBJECT_ID('HumanResources.uspUpdateEmployeePersonalInfo')
+GO
+
+sp_depends 'HumanResources.uspUpdateEmployeePersonalInfo'
+GO
+
+USE AdventureWorks2022
+GO
+--Tạo thủ tục lưu trữ sp_DisplayEmployees
+CREATE PROCEDURE sp_DisplayEmployees AS
+SELECT * FROM HumanResources.Employee
+GO
+
+ALTER PROCEDURE sp_DisplayEmployees AS
+SELECT * FROM HumanResources.Employee
+WHERE Gender='F'
+GO
+--Chạy thủ tục lưu trữ sp_DisplayEmployees
+EXEC sp_DisplayEmployees
+GO
+--Xóa một thủ tục lưu trữ
+DROP PROCEDURE sp_DisplayEmployees
+GO
+--
+CREATE PROCEDURE sp_EmployeeHire
+AS
+BEGIN
+--Hiển thị
+EXECUTE sp_DisplayEmployeesHireYear 1999
+DECLARE @Number int
+EXECUTE sp_EmployeesHireYearCount 1999, @Number OUTPUT
+PRINT N'Số nhân viên vào làm năm 1999 là: ' + 
+CONVERT(varchar(3),@Number)
+END
+GO
+--Chạy thủ tục lưu trữ
+EXEC sp_EmployeeHire 
+GO
+
+ALTER PROCEDURE sp_EmployeeHire
+@HireYear int
+AS
+BEGIN
+BEGIN TRY
+EXECUTE sp_DisplayEmployeesHireYear @HireYear
+DECLARE @Number int
+--Lỗi xảy ra ở đây có thủ tục sp_EmployeesHireYearCount chỉ
+truyền 2 tham số mà ta truyền 3
+EXECUTE sp_EmployeesHireYearCount @HireYear, @Number OUTPUT, 
+'123'
+PRINT N'Số nhân viên vào làm năm là: ' + 
+CONVERT(varchar(3),@Number)
+END TRY
+BEGIN CATCH
+PRINT N'Có lỗi xảy ra trong khi thực hiện thủ tục lưu trữ'
+END CATCH
+PRINT N'Kết thúc thủ tục lưu trữ'
+END
+GO
+
+EXEC sp_EmployeeHire 1999
+--Xem thông báo lỗi bên Messages không phải bên Result
+GO
+--Thay đổi thủ tục lưu trữ sp_EmployeeHire sử dụng hàm @@ERROR
+ALTER PROCEDURE sp_EmployeeHire
+@HireYear int
+AS
+BEGIN
+EXECUTE sp_DisplayEmployeesHireYear @HireYear
+DECLARE @Number int
+EXECUTE sp_EmployeesHireYearCount @HireYear, @Number OUTPUT, 
+'123'
+IF @@ERROR <> 0
+PRINT N'Có lỗi xảy ra trong khi thực hiện thủ tục lưu trữ'
+PRINT N'Số nhân viên vào làm năm là: ' + 
+CONVERT(varchar(3),@Number)
+END
+GO
+--Chạy thủ tục sp_EmployeeHire
+EXEC sp_EmployeeHire 1999
+
+
